26247bd16463c55a260570cd37abc2be4f960c46
artekm <44265668+artekm@users.noreply.github.com>
2019-06-30 08:50:42 +0200

    Small refactoring of ScheduleGenerator

--- src/main/java/pl/itacademy/schedule/generator/ScheduleGenerator.java
+++ src/main/java/pl/itacademy/schedule/generator/ScheduleGenerator.java
@@ -5,27 +5,32 @@ import pl.itacademy.schedule.parameters.EnteredParameters;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class ScheduleGenerator {
+	private Collection<DayOfWeek> lessonDays;
 
 	public Schedule generate(EnteredParameters parameters) {
 
 		Collection<Lesson> lessons = new ArrayList<>();
 		Schedule schedule = new Schedule();
 
-		long dailyMinutes = Duration.between(parameters.getBeginTime(), parameters.getEndTime()).toMinutes();
+		LocalTime beginTime = parameters.getBeginTime();
+		LocalTime endTime = parameters.getEndTime();
+		lessonDays = parameters.getLessonDays();
+
+		long dailyMinutes = Duration.between(beginTime, endTime).toMinutes();
 		long totalMinutes = parameters.getHoursNumber() * 60;
-		LocalDate startDate = findNextDate(parameters.getStartDate(), parameters.getLessonDays());
+		LocalDate startDate = findNextDate(parameters.getStartDate());
 		while (totalMinutes >= dailyMinutes) {
-			lessons.add(new Lesson(startDate, parameters.getBeginTime(), parameters.getEndTime()));
+			lessons.add(new Lesson(startDate, beginTime, endTime));
 			totalMinutes -= dailyMinutes;
-			startDate = findNextDate(startDate.plusDays(1), parameters.getLessonDays());
+			startDate = findNextDate(startDate.plusDays(1));
 		}
 		if (totalMinutes > 0) {
-			lessons.add(new Lesson(startDate, parameters.getBeginTime(),
-					parameters.getBeginTime().plusMinutes(totalMinutes)));
+			lessons.add(new Lesson(startDate, beginTime, beginTime.plusMinutes(totalMinutes)));
 			schedule.setLastDayShorter(true);
 		}
 		schedule.setLessons(lessons);
@@ -34,14 +39,14 @@ public class ScheduleGenerator {
 		return schedule;
 	}
 
-	private LocalDate findNextDate(LocalDate startDate, Collection<DayOfWeek> daysOfWeek) {
-		while (notLessonsDayOfWeek(daysOfWeek, startDate)) {
+	private LocalDate findNextDate(LocalDate startDate) {
+		while (notLessonsDayOfWeek(startDate)) {
 			startDate = startDate.plusDays(1);
 		}
 		return startDate;
 	}
 
-	private boolean notLessonsDayOfWeek(Collection<DayOfWeek> daysOfWeek, LocalDate date) {
-		return !daysOfWeek.contains(date.getDayOfWeek());
+	private boolean notLessonsDayOfWeek(LocalDate date) {
+		return !lessonDays.contains(date.getDayOfWeek());
 	}
 }
