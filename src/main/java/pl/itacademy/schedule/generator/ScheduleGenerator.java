eed7fe2084235bccc8712c315456cae6a0d3d62d
artekm <44265668+artekm@users.noreply.github.com>
2019-07-07 12:12:32 +0200

    Updates

--- src/main/java/pl/itacademy/schedule/generator/ScheduleGenerator.java
+++ src/main/java/pl/itacademy/schedule/generator/ScheduleGenerator.java
@@ -24,6 +24,17 @@ public class ScheduleGenerator {
 		long dailyMinutes = Duration.between(beginTime, endTime).toMinutes();
 		long totalMinutes = parameters.getHoursNumber() * 60;
 
+//		Use below code to get holidays from calendarific
+//		Collection<LocalDate> publicHolidays = webClient.getHolidays(parameters.getStartDate().getYear());
+//		try {
+//			Thread.sleep(1500);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		Collection<LocalDate> publicHolidays2 = webClient.getHolidays(parameters.getStartDate().plusYears(1).getYear());
+//		publicHolidays.addAll(publicHolidays2);
+
+//		Use below code to get holidays from Enrico
 		Collection<LocalDate> publicHolidays = webClient.getHolidaysFromEnrico(parameters.getStartDate(),
 				parameters.getStartDate().plusYears(1));
 
@@ -57,5 +68,50 @@ public class ScheduleGenerator {
 
 	private boolean isPublicHoliday(LocalDate date, Collection<LocalDate> publicHolidays) {
 		return publicHolidays.contains(date);
+	}
+
+	// This is unused so far, but maybe sometime ...
+	public boolean isHolidayByRule(LocalDate date) {
+		int day = date.getDayOfMonth();
+		int mon = date.getMonthValue();
+		if ((day == 1) && (mon == 1))
+			return true; // Nowy Rok
+		if ((day == 6) && (mon == 1))
+			return true; // 3 Królów
+		if ((day == 1) && (mon == 5))
+			return true; // Święto Pracy
+		if ((day == 3) && (mon == 5))
+			return true; // Dzień Konstytucji
+		if ((day == 15) && (mon == 8))
+			return true; // Wniebowzięcie NMP
+		if ((day == 1) && (mon == 11))
+			return true; // Wszystkich Świętych
+		if ((day == 11) && (mon == 11))
+			return true; // Dzień Niepodległości
+		if ((day == 25) && (mon == 12))
+			return true; // Boże Narodzenie 1
+		if ((day == 26) && (mon == 12))
+			return true; // Boże Narodzenie 2
+
+		int a = date.getYear() % 19;
+		int b = date.getYear() % 4;
+		int c = date.getYear() % 7;
+		int d = (a * 19 + 24) % 30;
+		int e = (2 * b + 4 * c + 6 * d + 5) % 7;
+		if (d == 29 && e == 6)
+			d -= 7;
+		if (d == 28 && e == 6 && a > 10)
+			d -= 7;
+		LocalDate easter = LocalDate.of(date.getYear(), 3, 22).plusDays(d + e);
+
+		if (easter.isEqual(date))
+			return true; // Wielkanoc
+		if (easter.plusDays(1).isEqual(date))
+			return true; // Wielkanoc poniedziałek
+		if (easter.plusDays(49).isEqual(date))
+			return true; // Zielone Świątki
+		if (easter.plusDays(60).isEqual(date))
+			return true; // Boże Ciało
+		return false;
 	}
 }
--- src/main/java/pl/itacademy/schedule/holidays/HolidaysWebClient.java
+++ src/main/java/pl/itacademy/schedule/holidays/HolidaysWebClient.java
@@ -6,62 +6,60 @@ import java.util.*;
 import java.util.stream.Collectors;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
-import com.jayway.jsonpath.*;
-import net.minidev.json.JSONArray;
+import com.jayway.jsonpath.JsonPath;
 import pl.itacademy.schedule.util.PropertiesReader;
 
 public class HolidaysWebClient {
 
-    public List<LocalDate> getHolidays(int year) {
+	public List<LocalDate> getHolidays(int year) {
 
-        Client client = ClientBuilder.newClient();
-        String url = PropertiesReader.getInstance().readProperty("calendarific.url");
-        String country = PropertiesReader.getInstance().readProperty("calendarific.country");
-        String type = PropertiesReader.getInstance().readProperty("calendarific.type");
-        String key = PropertiesReader.getInstance().readProperty("calendarific.api.key");
-        String jsonPath = PropertiesReader.getInstance().readProperty("calendarific.jsonPath");
-        WebTarget webTarget = client.target(url)
-                .queryParam("country", country)
-                .queryParam("type", type)
-                .queryParam("api_key", key)
-                .queryParam("year", year);
-        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		Client client = ClientBuilder.newClient();
+		String url = PropertiesReader.getInstance().readProperty("calendarific.url");
+		String country = PropertiesReader.getInstance().readProperty("calendarific.country");
+		String type = PropertiesReader.getInstance().readProperty("calendarific.type");
+		String key = PropertiesReader.getInstance().readProperty("calendarific.api.key");
+		String jsonPath = PropertiesReader.getInstance().readProperty("calendarific.jsonPath");
+		WebTarget webTarget = client.target(url)
+				.queryParam("country", country)
+				.queryParam("type", type)
+				.queryParam("api_key", key)
+				.queryParam("year", year);
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
 
-        String json = invocationBuilder.get(String.class);
+		String json = invocationBuilder.get(String.class);
 
-        DocumentContext context = JsonPath.parse(json);
-        JSONArray holidays = context.read(jsonPath);
-
-        return holidays.stream()
-                .map(holiday -> LocalDate.parse((String) holiday, DateTimeFormatter.ISO_DATE))
-                .collect(Collectors.toList());
-    }
-    
-    public List<LocalDate> getHolidaysFromEnrico(LocalDate startDate, LocalDate endDate) {
-
-        Client client = ClientBuilder.newClient();
-        PropertiesReader properties=PropertiesReader.getInstance();
-        String url = properties.readProperty("enrico.url");
-        String action = properties.readProperty("enrico.action");
-        String country = properties.readProperty("enrico.country");
-        String type = properties.readProperty("enrico.type");
-        String jsonPath = properties.readProperty("enrico.jsonPath");
-		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(properties.readProperty("enrico.dateFormat"));
-        
-        WebTarget webTarget = client.target(url)
-                .queryParam("action", action)
-                .queryParam("country", country)
-                .queryParam("type", type)
-                .queryParam("fromDate", startDate.format(dateFormatter))
-        		.queryParam("toDate", endDate.format(dateFormatter));
-        
-        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-        String json = invocationBuilder.get(String.class);
-
-		List<Map<String,Integer>> holidays = JsonPath.parse(json).read(jsonPath);
+		List<String> holidays = JsonPath.parse(json).read(jsonPath);
 
 		return holidays.stream()
-				.map(holiday->LocalDate.of(holiday.get("year"),holiday.get("month"),holiday.get("day")))
+				.map(holiday -> LocalDate.parse(holiday, DateTimeFormatter.ISO_DATE))
 				.collect(Collectors.toList());
-    }
+	}
+
+	public List<LocalDate> getHolidaysFromEnrico(LocalDate startDate, LocalDate endDate) {
+
+		Client client = ClientBuilder.newClient();
+		PropertiesReader properties = PropertiesReader.getInstance();
+		String url = properties.readProperty("enrico.url");
+		String action = properties.readProperty("enrico.action");
+		String country = properties.readProperty("enrico.country");
+		String type = properties.readProperty("enrico.type");
+		String jsonPath = properties.readProperty("enrico.jsonPath");
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(properties.readProperty("enrico.dateFormat"));
+
+		WebTarget webTarget = client.target(url)
+				.queryParam("action", action)
+				.queryParam("country", country)
+				.queryParam("type", type)
+				.queryParam("fromDate", startDate.format(dateFormatter))
+				.queryParam("toDate", endDate.format(dateFormatter));
+
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		String json = invocationBuilder.get(String.class);
+
+		List<Map<String, Integer>> holidays = JsonPath.read(json, jsonPath);
+
+		return holidays.stream()
+				.map(holiday -> LocalDate.of(holiday.get("year"), holiday.get("month"), holiday.get("day")))
+				.collect(Collectors.toList());
+	}
 }
