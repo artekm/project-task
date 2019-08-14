430266e1d184aef7dbda13e7dc21f1d1ba4e0570
artekm <44265668+artekm@users.noreply.github.com>
2019-08-14 22:29:59 +0200

    Refactored

--- src/main/java/pl/itacademy/schedule/ScheduleGeneratorApp.java
+++ src/main/java/pl/itacademy/schedule/ScheduleGeneratorApp.java
@@ -1,7 +1,16 @@
 package pl.itacademy.schedule;
 
+import java.awt.EventQueue;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.format.DateTimeParseException;
+
 import org.apache.commons.cli.ParseException;
 import org.apache.poi.ss.usermodel.Workbook;
+
 import pl.itacademy.schedule.exception.IncorrectParametersException;
 import pl.itacademy.schedule.generator.ExcelCreator;
 import pl.itacademy.schedule.generator.Schedule;
@@ -14,78 +23,117 @@ import pl.itacademy.schedule.parameters.ParametersReader;
 import pl.itacademy.schedule.parameters.ParametersValidator;
 import pl.itacademy.schedule.util.PropertiesReader;
 
-import java.awt.EventQueue;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.format.DateTimeParseException;
-
 public class ScheduleGeneratorApp {
 	public static final String WARNING_MESSAGE = "Warning: last lesson is shorter than the others\n";
 
-	public static void main(String[] args) {
+	private EnteredParameters parameters;
+	private Schedule schedule;
+	private Workbook workbook;
 
-		if (args.length==0) {
-			EventQueue.invokeLater(() -> new GUI().runGUI());
+	public EnteredParameters getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(EnteredParameters parameters) {
+		this.parameters = parameters;
+	}
+
+	public Schedule getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(Schedule schedule) {
+		this.schedule = schedule;
+	}
+
+	public void run(String[] args) {
+
+		if (args.length == 0) {
+			EventQueue.invokeLater(() -> new GUI(this).runGUI());
 			return;
 		}
+
+		if (readParameters(args)) {
+			generateSchedule();
+			generateExcel();
+			saveExcelToFile(null);
+		}
+	}
+
+	public static void main(String[] args) {
+		ScheduleGeneratorApp app = new ScheduleGeneratorApp();
+		app.run(args);
+	}
+
+	public boolean readParameters(String[] args) {
 		ParametersReader parametersReader = new ParametersReader();
 		ParametersReader.UsagePrinter usagePrinter = parametersReader.new UsagePrinter();
-		EnteredParameters enteredParameters;
 		try {
-			enteredParameters = parametersReader.parseArguments(args);
-			if (enteredParameters.isShowHelp()) {
+			parameters = parametersReader.parseArguments(args);
+			if (parameters.isShowHelp()) {
 				usagePrinter.printHelp();
-				return;
+				return false;
 			}
 			ParametersValidator validator = new ParametersValidator();
-			validator.validate(enteredParameters);
+			validator.validate(parameters);
 		} catch (NumberFormatException e) {
 			System.out.println("Impossible to read number " + e.getMessage());
 			usagePrinter.printHelp();
-			return;
+			return false;
 		} catch (IncorrectParametersException | ParseException | DateTimeParseException | IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			usagePrinter.printHelp();
-			return;
+			return false;
 		}
+		return true;
+	}
+
+	public boolean generateSchedule() {
 		HolidaysProvider webClient = HolidaysProviderFactory.getProvider();
-		
+
 		ScheduleGenerator scheduleGenerator = new ScheduleGenerator(webClient);
-		Schedule schedule = scheduleGenerator.generate(enteredParameters);
+		schedule = scheduleGenerator.generate(parameters);
+		return true;
+	}
 
-		String fileName;
-		if (enteredParameters.getFileName() == null) {
-			PropertiesReader propertiesReader = PropertiesReader.getInstance();
-			fileName = propertiesReader.readProperty("excel.defaultName");
-		} else {
-			fileName = enteredParameters.getFileName();
-		}
-		if (!fileName.endsWith(".xlsx")) {
-			fileName = fileName + ".xlsx";
-		}
-
+	public boolean generateExcel() {
 		ExcelCreator excelCreator = new ExcelCreator();
+		workbook = excelCreator.createWorkbook(schedule);
+		return true;
+	}
 
-
-
-		try(OutputStream stream = new FileOutputStream(fileName);
-			Workbook workbook = excelCreator.createWorkbook(schedule)) {
+	public boolean saveExcelToFile(String file) {
+		String fileName;
+		if (file != null) {
+			fileName = file;
+		} else {
+			if (parameters.getFileName() == null) {
+				PropertiesReader propertiesReader = PropertiesReader.getInstance();
+				fileName = propertiesReader.readProperty("excel.defaultName");
+			} else {
+				fileName = parameters.getFileName();
+			}
+			if (!fileName.endsWith(".xlsx")) {
+				fileName = fileName + ".xlsx";
+			}
+		}
+		try (OutputStream stream = new FileOutputStream(fileName)) {
 			workbook.write(stream);
 			System.out.println("Successfully saved the schedule to file " + fileName);
 		} catch (IOException e) {
 			System.out.println("Impossible to write schedule workbook.");
-			return;
+			return false;
 		}
 
-		if(schedule.isLastDayShorter()){
+		if (schedule.isLastDayShorter()) {
 			System.out.println(WARNING_MESSAGE);
 			try {
 				String shortName = fileName.substring(0, fileName.lastIndexOf("."));
 				Files.write(Paths.get(shortName + "-warning.txt"), WARNING_MESSAGE.getBytes());
-			} catch (IOException ignored) {}
+			} catch (IOException ignored) {
+			}
 		}
+		return true;
 	}
+
 }
